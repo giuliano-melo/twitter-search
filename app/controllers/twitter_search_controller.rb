5bc0c13a2b11d454abc39c6d4643fe6a5b8539ae
Giuliano Melo <giuliano.melo@abril.com.br>
2014-09-06 02:00:13 -0300

    search term available on view

--- app/controllers/twitter_search_controller.rb
+++ app/controllers/twitter_search_controller.rb
@@ -1,12 +1,12 @@
 class TwitterSearchController < ApplicationController
   def index
-    search_term = params['term'] || 'love mondays'
+    @search_term = params['term'] || 'love mondays'
 
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['twitter_config_key']
       config.consumer_secret     = ENV['twitter_config_secret']
     end
     params = { result_type: 'recent', show_user: true, rpp: 20 }
-    @tweets = client.search(search_term, params).select { |t| t.geo.present? }
+    @tweets = client.search(@search_term, params).select { |t| t.geo.present? }
   end
 end
